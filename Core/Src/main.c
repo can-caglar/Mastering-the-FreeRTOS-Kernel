c59fbf702c66afecb783c80b1038b2f3bc77d0ad
Can Caglar <charlerjan@gmail.com>
2022-12-27 10:03:38 +0000

    remove redundant code. add console io

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -1,592 +1,132 @@
-/* USER CODE BEGIN Header */
-/**
-  ******************************************************************************
-  * @file           : main.c
-  * @brief          : Main program body
-  ******************************************************************************
-  * @attention
-  *
-  * Copyright (c) 2022 STMicroelectronics.
-  * All rights reserved.
-  *
-  * This software is licensed under terms that can be found in the LICENSE file
-  * in the root directory of this software component.
-  * If no LICENSE file comes with this software, it is provided AS-IS.
-  *
-  ******************************************************************************
-  */
-/* USER CODE END Header */
-/* Includes ------------------------------------------------------------------*/
-#include "main.h"
-#include "cmsis_os.h"
-#include "usb_host.h"
+#include "FreeRTOS.h"
+#include "task.h"
+#include "queue.h"
+#include <stdio.h>
 
-/* Private includes ----------------------------------------------------------*/
-/* USER CODE BEGIN Includes */
+// Tasks
+void vTask1(void* vParam);
+void vTaskSendToQueue(void* vParam);
+void vTaskReceiveFromQueue(void* vParam);
 
-/* USER CODE END Includes */
+// Generic Data
+const int MyNumbers[] = {100, 200};
+const char* const MyCharacters[] = {"hello", "my", "old", "friend"};
 
-/* Private typedef -----------------------------------------------------------*/
-/* USER CODE BEGIN PTD */
-
-/* USER CODE END PTD */
-
-/* Private define ------------------------------------------------------------*/
-/* USER CODE BEGIN PD */
-/* USER CODE END PD */
-
-/* Private macro -------------------------------------------------------------*/
-/* USER CODE BEGIN PM */
-
-/* USER CODE END PM */
-
-/* Private variables ---------------------------------------------------------*/
-I2C_HandleTypeDef hi2c1;
-
-I2S_HandleTypeDef hi2s3;
-
-SPI_HandleTypeDef hspi1;
-
-osThreadId defaultTaskHandle;
-osThreadId myTask02Handle;
-/* USER CODE BEGIN PV */
-
-#define mainDELAY_LOOP_COUNT 500000
-
-/* USER CODE END PV */
-
-/* Private function prototypes -----------------------------------------------*/
-/* USER CODE BEGIN PFP */
-void SystemClock_Config(void);
-static void MX_GPIO_Init(void);
-static void MX_I2C1_Init(void);
-static void MX_I2S3_Init(void);
-static void MX_SPI1_Init(void);
-void StartDefaultTask(void const * argument);
-void StartTask02(void const * argument);
-
-void vTask1(void* pvParameters);
-void vTaskTrafficLights(void* pvParameters);
-void vTaskConstant(void* pvParameters);
-
-typedef struct MyLED
+typedef enum TaskId_e
 {
-  GPIO_TypeDef *GPIO_Port;
-  uint16_t GPIO_Pin;
-} MyLED;
+  TASK1_DATA,
+  TASK2_DATA
+} TaskId_e;
 
-typedef struct OnOffPeriod
+struct Data_s
 {
-  TickType_t on;
-  TickType_t off;
-} OnOffPeriod;
-
-typedef struct TrafficLightData
+  TaskId_e id;
+  void* value;
+} structData[2] =
 {
-  MyLED led;
-  OnOffPeriod period;
-  uint8_t start;
-} TrafficLightData;
-
-const MyLED greenLed = {.GPIO_Port = LD4_GPIO_Port, .GPIO_Pin = LD4_Pin};
-const MyLED blueLed = {.GPIO_Port = LD6_GPIO_Port, .GPIO_Pin = LD6_Pin};
-const MyLED orangeLed = {.GPIO_Port = LD3_GPIO_Port, .GPIO_Pin = LD3_Pin};
-const MyLED redLed = {.GPIO_Port = LD5_GPIO_Port, .GPIO_Pin = LD5_Pin};
-
-TrafficLightData redTrafficLight;
-TrafficLightData amberTrafficLight;
-TrafficLightData greenTrafficLight;
-
-void vApplicationIdleHook(void);
+  {TASK1_DATA, (void*)&MyNumbers[0]},
+  {TASK2_DATA, (void*)&MyCharacters[0]},
+};
+typedef struct Data_s Data_s;
 
 
-/* USER CODE END PFP */
+// FreeRTOS Objects
 
-/* Private user code ---------------------------------------------------------*/
-/* USER CODE BEGIN 0 */
+// Queues
+xQueueHandle myQueue;
 
-/* USER CODE END 0 */
+void Error_Handler()
+{
+  printf("error!\n");
+  while(1)
+  {
+  }
+}
 
-/**
-  * @brief  The application entry point.
-  * @retval int
-  */
 int main(void)
 {
-  /* USER CODE BEGIN 1 */
-
-  /* USER CODE END 1 */
-
-  /* MCU Configuration--------------------------------------------------------*/
-
-  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
-  HAL_Init();
-
-  /* USER CODE BEGIN Init */
-
-  /* USER CODE END Init */
-
-  /* Configure the system clock */
-  SystemClock_Config();
-
-  /* USER CODE BEGIN SysInit */
-
-  /* USER CODE END SysInit */
-
-  /* Initialize all configured peripherals */
-  MX_GPIO_Init();
-  MX_I2C1_Init();
-  MX_I2S3_Init();
-  MX_SPI1_Init();
-  /* USER CODE BEGIN 2 */
-
-
-
-  /* USER CODE END 2 */
-
-  /* USER CODE BEGIN RTOS_MUTEX */
-  /* add mutexes, ... */
-  /* USER CODE END RTOS_MUTEX */
-
-  /* USER CODE BEGIN RTOS_SEMAPHORES */
-  /* add semaphores, ... */
-  /* USER CODE END RTOS_SEMAPHORES */
-
-  /* USER CODE BEGIN RTOS_TIMERS */
-  /* start timers, add new ones, ... */
-  /* USER CODE END RTOS_TIMERS */
-
-  /* USER CODE BEGIN RTOS_QUEUES */
-  /* add queues, ... */
-  /* USER CODE END RTOS_QUEUES */
+  printf("Program starting...\n");
   
-  #if 0
-  xTaskCreate(vTask1, "vTask1", 100, (void*)&greenLed, 2, NULL);
-  xTaskCreate(vTaskConstant, "vTaskConstant", 100, (void*)&blueLed, 1, NULL);
-  xTaskCreate(vTaskConstant, "vTaskConstant", 100, (void*)&orangeLed, 1, NULL);
-  xTaskCreate(vTaskConstant, "vTaskConstant", 100, (void*)&redLed, 1, NULL);
-  #endif
+  // Create FreeRTOS objects
+  myQueue = xQueueCreate(5, sizeof(Data_s));
   
-  // traffic light app
-  redTrafficLight.led = redLed;
-  redTrafficLight.period.on = 5000;
-  redTrafficLight.period.off = 7000;
-  redTrafficLight.start = 1;
- 
-  amberTrafficLight.led = orangeLed;
-  amberTrafficLight.period.on = 1000;
-  amberTrafficLight.period.off = 5000;
-  amberTrafficLight.start = 0;
-
-  greenTrafficLight.led = greenLed;
-  greenTrafficLight.period.on = 6000;
-  greenTrafficLight.period.off = 6000;
-  greenTrafficLight.start = 0;
-
-  xTaskCreate(vTaskTrafficLights, "redTrafficLight", 100, (void*)&redTrafficLight, 2, NULL);
-  xTaskCreate(vTaskTrafficLights, "amberTrafficLight", 100, (void*)&amberTrafficLight, 2, NULL);
-  xTaskCreate(vTaskTrafficLights, "greenTrafficLight", 100, (void*)&greenTrafficLight, 2, NULL);
-  // end
+  // Task creation
+  xTaskCreate(vTaskSendToQueue, "TaskSend1", 0x100, &structData[0], 2, NULL);
+  xTaskCreate(vTaskSendToQueue, "TaskSend2", 0x100, &structData[1], 2, NULL);
+  xTaskCreate(vTaskReceiveFromQueue, "TaskRecv", 0x100, NULL, 1, NULL);
   
+  // Begin scheduler
   vTaskStartScheduler();
-
-  /* USER CODE BEGIN RTOS_THREADS */
-  /* add threads, ... */
-  /* USER CODE END RTOS_THREADS */
-
-
-
-  /* We should never get here as control is now taken by the scheduler */
-  /* Infinite loop */
-  /* USER CODE BEGIN WHILE */
-  while (1)
-  {
-    /* USER CODE END WHILE */
-
-    /* USER CODE BEGIN 3 */
-  }
-  /* USER CODE END 3 */
-}
-
-/**
-  * @brief System Clock Configuration
-  * @retval None
-  */
-void SystemClock_Config(void)
-{
-  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
-  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
-
-  /** Configure the main internal regulator output voltage
-  */
-  __HAL_RCC_PWR_CLK_ENABLE();
-  __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
-
-  /** Initializes the RCC Oscillators according to the specified parameters
-  * in the RCC_OscInitTypeDef structure.
-  */
-  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
-  RCC_OscInitStruct.HSEState = RCC_HSE_ON;
-  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
-  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
-  RCC_OscInitStruct.PLL.PLLM = 8;
-  RCC_OscInitStruct.PLL.PLLN = 336;
-  RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
-  RCC_OscInitStruct.PLL.PLLQ = 7;
-  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
-  {
-    Error_Handler();
-  }
-
-  /** Initializes the CPU, AHB and APB buses clocks
-  */
-  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
-                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
-  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
-  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
-  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV4;
-  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV2;
-
-  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_5) != HAL_OK)
-  {
-    Error_Handler();
-  }
-}
-
-/**
-  * @brief I2C1 Initialization Function
-  * @param None
-  * @retval None
-  */
-static void MX_I2C1_Init(void)
-{
-
-  /* USER CODE BEGIN I2C1_Init 0 */
-
-  /* USER CODE END I2C1_Init 0 */
-
-  /* USER CODE BEGIN I2C1_Init 1 */
-
-  /* USER CODE END I2C1_Init 1 */
-  hi2c1.Instance = I2C1;
-  hi2c1.Init.ClockSpeed = 100000;
-  hi2c1.Init.DutyCycle = I2C_DUTYCYCLE_2;
-  hi2c1.Init.OwnAddress1 = 0;
-  hi2c1.Init.AddressingMode = I2C_ADDRESSINGMODE_7BIT;
-  hi2c1.Init.DualAddressMode = I2C_DUALADDRESS_DISABLE;
-  hi2c1.Init.OwnAddress2 = 0;
-  hi2c1.Init.GeneralCallMode = I2C_GENERALCALL_DISABLE;
-  hi2c1.Init.NoStretchMode = I2C_NOSTRETCH_DISABLE;
-  if (HAL_I2C_Init(&hi2c1) != HAL_OK)
-  {
-    Error_Handler();
-  }
-  /* USER CODE BEGIN I2C1_Init 2 */
-
-  /* USER CODE END I2C1_Init 2 */
-
-}
-
-/**
-  * @brief I2S3 Initialization Function
-  * @param None
-  * @retval None
-  */
-static void MX_I2S3_Init(void)
-{
-
-  /* USER CODE BEGIN I2S3_Init 0 */
-
-  /* USER CODE END I2S3_Init 0 */
-
-  /* USER CODE BEGIN I2S3_Init 1 */
-
-  /* USER CODE END I2S3_Init 1 */
-  hi2s3.Instance = SPI3;
-  hi2s3.Init.Mode = I2S_MODE_MASTER_TX;
-  hi2s3.Init.Standard = I2S_STANDARD_PHILIPS;
-  hi2s3.Init.DataFormat = I2S_DATAFORMAT_16B;
-  hi2s3.Init.MCLKOutput = I2S_MCLKOUTPUT_ENABLE;
-  hi2s3.Init.AudioFreq = I2S_AUDIOFREQ_96K;
-  hi2s3.Init.CPOL = I2S_CPOL_LOW;
-  hi2s3.Init.ClockSource = I2S_CLOCK_PLL;
-  hi2s3.Init.FullDuplexMode = I2S_FULLDUPLEXMODE_DISABLE;
-  if (HAL_I2S_Init(&hi2s3) != HAL_OK)
-  {
-    Error_Handler();
-  }
-  /* USER CODE BEGIN I2S3_Init 2 */
-
-  /* USER CODE END I2S3_Init 2 */
-
-}
-
-/**
-  * @brief SPI1 Initialization Function
-  * @param None
-  * @retval None
-  */
-static void MX_SPI1_Init(void)
-{
-
-  /* USER CODE BEGIN SPI1_Init 0 */
-
-  /* USER CODE END SPI1_Init 0 */
-
-  /* USER CODE BEGIN SPI1_Init 1 */
-
-  /* USER CODE END SPI1_Init 1 */
-  /* SPI1 parameter configuration*/
-  hspi1.Instance = SPI1;
-  hspi1.Init.Mode = SPI_MODE_MASTER;
-  hspi1.Init.Direction = SPI_DIRECTION_2LINES;
-  hspi1.Init.DataSize = SPI_DATASIZE_8BIT;
-  hspi1.Init.CLKPolarity = SPI_POLARITY_LOW;
-  hspi1.Init.CLKPhase = SPI_PHASE_1EDGE;
-  hspi1.Init.NSS = SPI_NSS_SOFT;
-  hspi1.Init.BaudRatePrescaler = SPI_BAUDRATEPRESCALER_2;
-  hspi1.Init.FirstBit = SPI_FIRSTBIT_MSB;
-  hspi1.Init.TIMode = SPI_TIMODE_DISABLE;
-  hspi1.Init.CRCCalculation = SPI_CRCCALCULATION_DISABLE;
-  hspi1.Init.CRCPolynomial = 10;
-  if (HAL_SPI_Init(&hspi1) != HAL_OK)
-  {
-    Error_Handler();
-  }
-  /* USER CODE BEGIN SPI1_Init 2 */
-
-  /* USER CODE END SPI1_Init 2 */
-
-}
-
-/**
-  * @brief GPIO Initialization Function
-  * @param None
-  * @retval None
-  */
-static void MX_GPIO_Init(void)
-{
-  GPIO_InitTypeDef GPIO_InitStruct = {0};
-
-  /* GPIO Ports Clock Enable */
-  __HAL_RCC_GPIOE_CLK_ENABLE();
-  __HAL_RCC_GPIOC_CLK_ENABLE();
-  __HAL_RCC_GPIOH_CLK_ENABLE();
-  __HAL_RCC_GPIOA_CLK_ENABLE();
-  __HAL_RCC_GPIOB_CLK_ENABLE();
-  __HAL_RCC_GPIOD_CLK_ENABLE();
-
-  /*Configure GPIO pin Output Level */
-  HAL_GPIO_WritePin(CS_I2C_SPI_GPIO_Port, CS_I2C_SPI_Pin, GPIO_PIN_RESET);
-
-  /*Configure GPIO pin Output Level */
-  HAL_GPIO_WritePin(OTG_FS_PowerSwitchOn_GPIO_Port, OTG_FS_PowerSwitchOn_Pin, GPIO_PIN_SET);
-
-  /*Configure GPIO pin Output Level */
-  HAL_GPIO_WritePin(GPIOD, LD4_Pin|LD3_Pin|LD5_Pin|LD6_Pin
-                          |Audio_RST_Pin, GPIO_PIN_RESET);
-
-  /*Configure GPIO pin : CS_I2C_SPI_Pin */
-  GPIO_InitStruct.Pin = CS_I2C_SPI_Pin;
-  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
-  GPIO_InitStruct.Pull = GPIO_NOPULL;
-  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
-  HAL_GPIO_Init(CS_I2C_SPI_GPIO_Port, &GPIO_InitStruct);
-
-  /*Configure GPIO pin : OTG_FS_PowerSwitchOn_Pin */
-  GPIO_InitStruct.Pin = OTG_FS_PowerSwitchOn_Pin;
-  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
-  GPIO_InitStruct.Pull = GPIO_NOPULL;
-  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
-  HAL_GPIO_Init(OTG_FS_PowerSwitchOn_GPIO_Port, &GPIO_InitStruct);
-
-  /*Configure GPIO pin : PDM_OUT_Pin */
-  GPIO_InitStruct.Pin = PDM_OUT_Pin;
-  GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
-  GPIO_InitStruct.Pull = GPIO_NOPULL;
-  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
-  GPIO_InitStruct.Alternate = GPIO_AF5_SPI2;
-  HAL_GPIO_Init(PDM_OUT_GPIO_Port, &GPIO_InitStruct);
-
-  /*Configure GPIO pin : B1_Pin */
-  GPIO_InitStruct.Pin = B1_Pin;
-  GPIO_InitStruct.Mode = GPIO_MODE_EVT_RISING;
-  GPIO_InitStruct.Pull = GPIO_NOPULL;
-  HAL_GPIO_Init(B1_GPIO_Port, &GPIO_InitStruct);
-
-  /*Configure GPIO pin : BOOT1_Pin */
-  GPIO_InitStruct.Pin = BOOT1_Pin;
-  GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
-  GPIO_InitStruct.Pull = GPIO_NOPULL;
-  HAL_GPIO_Init(BOOT1_GPIO_Port, &GPIO_InitStruct);
-
-  /*Configure GPIO pin : CLK_IN_Pin */
-  GPIO_InitStruct.Pin = CLK_IN_Pin;
-  GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
-  GPIO_InitStruct.Pull = GPIO_NOPULL;
-  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
-  GPIO_InitStruct.Alternate = GPIO_AF5_SPI2;
-  HAL_GPIO_Init(CLK_IN_GPIO_Port, &GPIO_InitStruct);
-
-  /*Configure GPIO pins : LD4_Pin LD3_Pin LD5_Pin LD6_Pin
-                           Audio_RST_Pin */
-  GPIO_InitStruct.Pin = LD4_Pin|LD3_Pin|LD5_Pin|LD6_Pin
-                          |Audio_RST_Pin;
-  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
-  GPIO_InitStruct.Pull = GPIO_NOPULL;
-  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
-  HAL_GPIO_Init(GPIOD, &GPIO_InitStruct);
-
-  /*Configure GPIO pin : OTG_FS_OverCurrent_Pin */
-  GPIO_InitStruct.Pin = OTG_FS_OverCurrent_Pin;
-  GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
-  GPIO_InitStruct.Pull = GPIO_NOPULL;
-  HAL_GPIO_Init(OTG_FS_OverCurrent_GPIO_Port, &GPIO_InitStruct);
-
-  /*Configure GPIO pin : MEMS_INT2_Pin */
-  GPIO_InitStruct.Pin = MEMS_INT2_Pin;
-  GPIO_InitStruct.Mode = GPIO_MODE_EVT_RISING;
-  GPIO_InitStruct.Pull = GPIO_NOPULL;
-  HAL_GPIO_Init(MEMS_INT2_GPIO_Port, &GPIO_InitStruct);
-
-}
-
-/* USER CODE BEGIN 4 */
-
-/* USER CODE END 4 */
-
-/* USER CODE BEGIN Header_StartDefaultTask */
-/**
-  * @brief  Function implementing the defaultTask thread.
-  * @param  argument: Not used
-  * @retval None
-  */
-/* USER CODE END Header_StartDefaultTask */
-void StartDefaultTask(void const * argument)
-{
-  /* init code for USB_HOST */
-  MX_USB_HOST_Init();
-  /* USER CODE BEGIN 5 */
-  /* Infinite loop */
-  for(;;)
-  {
-    osDelay(1);
-  }
-  /* USER CODE END 5 */
-}
-
-/* USER CODE BEGIN Header_StartTask02 */
-/**
-* @brief Function implementing the myTask02 thread.
-* @param argument: Not used
-* @retval None
-*/
-/* USER CODE END Header_StartTask02 */
-void StartTask02(void const * argument)
-{
-  /* USER CODE BEGIN StartTask02 */
-  /* Infinite loop */
-  for(;;)
-  {
-    osDelay(1);
-  }
-  /* USER CODE END StartTask02 */
-}
-
-/**
-  * @brief  Period elapsed callback in non blocking mode
-  * @note   This function is called  when TIM1 interrupt took place, inside
-  * HAL_TIM_IRQHandler(). It makes a direct call to HAL_IncTick() to increment
-  * a global variable "uwTick" used as application time base.
-  * @param  htim : TIM handle
-  * @retval None
-  */
-void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim)
-{
-  /* USER CODE BEGIN Callback 0 */
-
-  /* USER CODE END Callback 0 */
-  if (htim->Instance == TIM1) {
-    HAL_IncTick();
-  }
-  /* USER CODE BEGIN Callback 1 */
-
-  /* USER CODE END Callback 1 */
-}
-
-/**
-  * @brief  This function is executed in case of error occurrence.
-  * @retval None
-  */
-void Error_Handler(void)
-{
-  /* USER CODE BEGIN Error_Handler_Debug */
-  /* User can add his own implementation to report the HAL error return state */
-  __disable_irq();
-  while (1)
-  {
-  }
-  /* USER CODE END Error_Handler_Debug */
-}
-
-void vTask1(void* pvParameters)
-{
-  MyLED* led = (MyLED*)pvParameters;
-  TickType_t xLastWakeTime = xTaskGetTickCount();
+  
+  // should never get to here
+  printf("Entering forever loop!\n");
   while(1)
   {
-    HAL_GPIO_WritePin(led->GPIO_Port, led->GPIO_Pin, GPIO_PIN_SET);
-    vTaskDelayUntil(&xLastWakeTime, pdMS_TO_TICKS(100));  // vTaskDelay(pdMS_TO_TICKS(100));
-
-    HAL_GPIO_WritePin(led->GPIO_Port, led->GPIO_Pin, GPIO_PIN_RESET);
-    vTaskDelayUntil(&xLastWakeTime, pdMS_TO_TICKS(100));  // vTaskDelay(pdMS_TO_TICKS(100));
   }
 }
 
-void vTaskConstant(void* pvParameters)
+void vTask1(void* vParam)
 {
-  MyLED* led = (MyLED*)pvParameters;
+  char* str = (char*)vParam;
   while(1)
   {
-    HAL_GPIO_WritePin(led->GPIO_Port, led->GPIO_Pin, GPIO_PIN_SET);
-    for (int i = 0; i < mainDELAY_LOOP_COUNT; i++)
+    printf("This is task: %s\n", str);
+    vTaskDelay(pdMS_TO_TICKS(500));
+  }
+}
+
+void vTaskSendToQueue(void* vParam)
+{
+  printf("vTaskSendToQueue - Starting sending task...\n");
+  
+  while(1)
+  {
+    BaseType_t err = xQueueSend(myQueue, vParam, pdMS_TO_TICKS(500));
+    if (err != errQUEUE_FULL)
     {
-      volatile int a = i;
-    }
-    HAL_GPIO_WritePin(led->GPIO_Port, led->GPIO_Pin, GPIO_PIN_RESET);
-    for (int i = 0; i < mainDELAY_LOOP_COUNT; i++)
-    {
-      volatile int a = i;
+      printf("vTaskSendToQueue - Queue is full\n");
     }
   }
 }
 
-void vTaskTrafficLights(void* pvParameters)
+void vTaskReceiveFromQueue(void* vParam)
 {
-  TrafficLightData* data = (TrafficLightData*)pvParameters;
-  TickType_t xLastWakeTime = xTaskGetTickCount();
-  // TickType_t xLastOffTime = xTaskGetTickCount();
-  uint8_t startOn = data->start;
+  printf("vTaskReceiveFromQueue - Starting...\n");
   while(1)
   {
-    if (startOn)
+    // Print info
+    printf("vTaskReceiveFromQueue Items in Q = %lu\n", uxQueueMessagesWaiting(myQueue));
+    
+    // Receive from queue
+    Data_s dataReceived;
+    BaseType_t err = xQueueReceive(myQueue, &dataReceived, pdMS_TO_TICKS(500));
+    
+    // Error checking and logging
+    if (err == pdPASS)
     {
-      HAL_GPIO_WritePin(data->led.GPIO_Port, data->led.GPIO_Pin, GPIO_PIN_SET);
-      vTaskDelayUntil(&xLastWakeTime, pdMS_TO_TICKS(data->period.on));
+      switch (dataReceived.id)
+      {
+        case TASK1_DATA:
+        {
+          printf("vTaskReceiveFromQueue - Received from TASK1, number: %d\n", (*(int*)dataReceived.value));
+        }
+        break;
+        case TASK2_DATA:
+        {
+          printf("vTaskReceiveFromQueue - Received from TASK2, string: %s\n", (*(char**)dataReceived.value));
+        }
+        break;
+        default:
+        {
+          printf("vTaskReceiveFromQueue - Unhandled ID\n");
+        }
+        break;
+      }
+      
     }
     else
     {
-      startOn = 1; // for next time
+      printf("vTaskReceiveFromQueue - Queue empty and timed out\n");
     }
-
-    HAL_GPIO_WritePin(data->led.GPIO_Port, data->led.GPIO_Pin, GPIO_PIN_RESET);
-    vTaskDelayUntil(&xLastWakeTime, pdMS_TO_TICKS(data->period.off));
   }
-}
-
-uint32_t idleCounter = 0;
-
-void vApplicationIdleHook(void)
-{
-  idleCounter++;
 }
