25c05c9f629b219bc61b664416d997292835a9eb
Can Caglar <charlerjan@gmail.com>
2022-12-28 10:36:05 +0000

    timer id example. using 1 callback for two timers. each does something different by checking the timer. each makes use of the unique id of the timer.

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -6,19 +6,20 @@
 #include <stdlib.h>
 
 // Creating a one-shot and auto reload timer
+// but only having 1 timer callback
+// and the auto-reload timer will stop after 5 executions.
 
 // Create empty timer objects
 TimerHandle_t osTimer;
 TimerHandle_t arTimer;
 
 void timerCallback1(TimerHandle_t xTimer);
-void timerCallback2(TimerHandle_t xTimer);
 
 int main(void)
 {
   // Create the timers
-  arTimer = xTimerCreate("Timer 1", pdMS_TO_TICKS(500), 1, NULL, timerCallback1);
-  osTimer = xTimerCreate("Timer 2", pdMS_TO_TICKS(1500), 0, NULL, timerCallback2);
+  arTimer = xTimerCreate("Timer 1", pdMS_TO_TICKS(500), pdTRUE, 0, timerCallback1);
+  osTimer = xTimerCreate("Timer 2", pdMS_TO_TICKS(1500), pdFALSE, 0, timerCallback1);
   
   // Start them
   volatile BaseType_t t1started = xTimerStart(arTimer, 0);
@@ -42,10 +43,23 @@ int main(void)
 
 void timerCallback1(TimerHandle_t xTimer)
 {
-  printf("Timer callback1 is being called\n");
-}
-
-void timerCallback2(TimerHandle_t xTimer)
-{
-  printf("Gundi! The callback2 is being called\n");
+  // Get the ID associated with it
+  int id = (int)pvTimerGetTimerID(xTimer);
+  int newID = id + 1;
+  vTimerSetTimerID(xTimer, (void*)newID);
+  
+  // Discern which timer it is
+  if (xTimer == osTimer)
+  {
+    printf("One shot timer executing! ID: %d\n", id);
+  }
+  else
+  {
+    printf("Auto-reload timer executing! ID: %d\n", id);
+    if (id > 5)
+    {
+      printf("Stopping auto-reload timer\n");
+      xTimerStop(xTimer, pdMS_TO_TICKS(500));
+    }
+  }
 }
