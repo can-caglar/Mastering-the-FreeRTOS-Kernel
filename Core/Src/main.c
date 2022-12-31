a3cfa81fdb9f8a8d2d68fe5097177a0339347ba3
Can Caglar <charlerjan@gmail.com>
2022-12-31 15:26:00 +0000

    event sync example via xEventGroupSync

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -3,36 +3,32 @@
 #include "task.h"
 #include "event_groups.h"
 #include "cc_button.h"
+#include <stdlib.h>
 
 /*
-This example demonstrates how to:
-- Create an event group.
-- Set bits in an event group from an interrupt service routine.
-- Set bits in an event group from a task.
-- Block on an event group
+This example demonstrates syncing of three tasks.
+It will print before and after the syncing happens to the 
+console.
 */
 
 EventGroupHandle_t xMyEventGroup = NULL;
 
-const EventBits_t buttonPress = (1 << 0);
-const EventBits_t printReady = (1 << 1);
+const EventBits_t xTaskBit1 = (1 << 0);
+const EventBits_t xTaskBit2 = (1 << 1);
+const EventBits_t xTaskBit3 = (1 << 2);
+const EventBits_t xAllBits = (xTaskBit1 | xTaskBit2 | xTaskBit3);
 
-void vTaskAllowPrint(void* pvParam);
-void vTaskPrint(void* pvParam);
+void vTaskSync(void* pvParam);
 
 int main(void)
 {
-  // An event group will be made.
-  // 1 bit will signify a button press,
-  // another bit will signify a task that will be
-  // used to print to console
-  
-  // Create FreeRTOS structures
+  // Create event group
   xMyEventGroup = xEventGroupCreate();
   
-  // Tasks... Daemon task is set to priority 2, so will preempt these
-  xTaskCreate(vTaskAllowPrint, "Allow Print", 0x100, NULL, 1, NULL);
-  xTaskCreate(vTaskPrint, "Print", 0x100, NULL, 1, NULL);
+  // Create tasks with varying flag params
+  xTaskCreate(vTaskSync, "vTaskSync1", 0x100, (void*)xTaskBit1, 1, NULL);
+  xTaskCreate(vTaskSync, "vTaskSync2", 0x100, (void*)xTaskBit2, 1, NULL);
+  xTaskCreate(vTaskSync, "vTaskSync3", 0x100, (void*)xTaskBit3, 1, NULL);
   
   // init hardware which will initialise interrupt
   button_init();
@@ -46,32 +42,22 @@ int main(void)
   }
 }
 
-void vTaskAllowPrint(void* pvParam)
+void vTaskSync(void* pvParam)
 {
-  // signify that we are ready to print
+  EventBits_t thisEventBits = (EventBits_t)pvParam;
+  const TickType_t minTime = pdMS_TO_TICKS(500);
+  const TickType_t range = pdMS_TO_TICKS(1000);
+  
   for (;;)
   {
-    xEventGroupSetBits(xMyEventGroup, printReady);
-    vTaskDelay(pdMS_TO_TICKS(2000));
-  }
-}
-
-void vTaskPrint(void* pvParam)
-{
-  const EventBits_t moneyEvent = (buttonPress | printReady);
-  static uint8_t timesPrinted = 0;
-  for (;;)
-  {
-    // print as soon as we're ready
-    EventBits_t eventsNow = xEventGroupWaitBits(xMyEventGroup, (buttonPress | printReady), pdTRUE, pdTRUE, pdMS_TO_TICKS(1000));
-    if ((eventsNow & moneyEvent) != moneyEvent)
-    {
-      printf("Timed out waiting for event groups. Flag now = 0x%X\n", eventsNow);
-    }
-    else
-    {
-      printf("vTaskPrint - Printing as both events (0x%x) arrived: %u\n", eventsNow, timesPrinted++);
-    }
+    // emulate task doing some operation
+    TickType_t randDelay = (rand() % range) + minTime;
+    vTaskDelay(randDelay);
+    
+    printf("Ready to sync (0x%x)\n", thisEventBits);
+    EventBits_t bits = xEventGroupSync(xMyEventGroup, thisEventBits, xAllBits, portMAX_DELAY);
+    printf("All synced up, leaving (0x%x)...\n", bits);
+    vTaskDelay(pdMS_TO_TICKS(1000));
   }
 }
 
@@ -86,16 +72,7 @@ void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin)
     
     if (tickNow > (lastBtnPress + pdMS_TO_TICKS(300)))  // debounce...
     {
-      tickNow = lastBtnPress;
       
-      // set event
-      BaseType_t pxHigherPriorityTaskWoken = pdFALSE;
-      xEventGroupSetBitsFromISR(xMyEventGroup, buttonPress, &pxHigherPriorityTaskWoken);
-      
-      if (pxHigherPriorityTaskWoken == pdTRUE)
-      {
-        portYIELD_FROM_ISR(pxHigherPriorityTaskWoken);  // yield to daemon task at the end of ISR call if possible
-      }
     }
   }
 }
